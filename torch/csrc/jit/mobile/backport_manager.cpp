808d4193ebce3471b9ae21379b97e48fc9b3b0d0
chenlai <chenlai@devvm4827.prn0.facebook.com>
2021-08-20 17:50:01 -0700

    [PyTorch Edge] Backport function for defaults args with out args, flag on
    1. Enable support for operators with default args and out args. For `torch.add(x, h, out=x)`, the number of specified arguments will be 3 instead of 4.
    2. Bump bytecode version from 6 to 7
    3. Implement backport_v7_to_v6 function. Also slightly refactor the local_thread to allow re-emit operators.
    4. unittest to cover backport function
    5. Update expect result from 4 to 3 in unit test DefaultArgsWithOutArg to cover the number of specified arguments.
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63651
    
    
    ghstack-source-id: 136367336
    
    Differential Revision: [D30454080](https://our.internmc.facebook.com/intern/diff/D30454080/)

--- torch/csrc/jit/mobile/backport_manager.cpp
+++ torch/csrc/jit/mobile/backport_manager.cpp
@@ -26,6 +26,7 @@ namespace {
 constexpr int64_t kBytecodeVersionV4 = 0x4L;
 constexpr int64_t kBytecodeVersionV5 = 0x5L;
 constexpr int64_t kBytecodeVersionV6 = 0x6L;
+constexpr int64_t kBytecodeVersionV7 = 0x7L;
 } // namespace
 
 // Utility function that can be reused by backport_vn_to_vn-1(). If any utility
@@ -296,7 +297,9 @@ std::stringstream backport_v6_to_v5(std::stringstream& input_model_stream) {
   // resolved at runtime init stage for better operator compatibility.
   std::stringstream intermediate_model_stream;
   {
-    BytecodeEmitDefaultInputsGuard argNumGuard(true);
+    BytecodeEmitModeGuard argNumGuard(
+        true /*emit_default_input_instructions*/,
+        false /*enable_defaults_args_with_out_args*/);
     torch_script._save_for_mobile(
         intermediate_model_stream, extra_files, hasBytecodeDebug);
   }
@@ -349,6 +352,95 @@ std::stringstream backport_v6_to_v5(std::stringstream& input_model_stream) {
 
   return ouput_model_stream;
 }
+
+std::stringstream backport_v7_to_v6(std::stringstream& input_model_stream) {
+  std::shared_ptr<IStreamAdapter> rai =
+      std::make_shared<IStreamAdapter>(&input_model_stream);
+  auto reader = std::make_shared<PyTorchStreamReader>(rai);
+  std::vector<IValue> constants_values =
+      readArchive(kArchiveNameConstants, *reader.get()).toTuple()->elements();
+
+  // If there are debug info files in the original model file, it should also
+  // show up in the backported model
+  bool hasBytecodeDebug = reader->hasRecord("mobile_debug_handles.pkl");
+
+  // extra_files are kept
+  auto records = reader->getAllRecords();
+  ExtraFilesMap extra_files;
+  for (const auto& record : records) {
+    std::size_t found = record.find_last_of("/\\");
+    auto path = record.substr(0, found);
+    if ("extra" == path) {
+      extra_files.emplace(record.substr(found + 1), "");
+    }
+  }
+  // Loading the TS module is required for this backport, because bytecode needs
+  // to be re-emitted (refer to the comments below)
+  Module torch_script = torch::jit::load(rai, c10::nullopt, extra_files);
+
+  // The RAII guard to change the flag, emit_default_input_instructions, to
+  // false to keep the same behavior in bytecode version 6. Change the flag,
+  // enable_defaults_args_with_out_args, to deserialized the number of specified
+  // operators which allowing both out arguments and default arguments to
+  // #all_args, instead of (#all_args - #default_args)
+  std::stringstream intermediate_model_stream;
+  {
+    BytecodeEmitModeGuard argNumGuard(
+        false /*emit_default_input_instructions*/,
+        false /*enable_defaults_args_with_out_args*/);
+    torch_script._save_for_mobile(
+        intermediate_model_stream, extra_files, hasBytecodeDebug);
+  }
+
+  // Update the bytecode version (from 7 to 6)
+
+  PyTorchStreamReader reader_bytecode(&intermediate_model_stream);
+  std::vector<IValue> bytecode_values = get_bytecode_ivalues(reader_bytecode);
+  std::unordered_set<std::string> excluded_files{
+      "constants.pkl",
+      "bytecode.pkl",
+      "version",
+  };
+
+  std::unordered_set<std::string> excluded_dirs{
+      "constants",
+      "bytecode",
+  };
+
+  std::stringstream ouput_model_stream;
+  auto writer_func = [&](const void* buf, size_t nbytes) -> size_t {
+    ouput_model_stream.write(static_cast<const char*>(buf), nbytes);
+    return !ouput_model_stream ? 0 : nbytes;
+  };
+
+  PyTorchStreamWriter writer_bytecode(writer_func);
+
+  selective_copy(
+      reader_bytecode, writer_bytecode, excluded_files, excluded_dirs);
+
+  update_bytecode_version(bytecode_values, kBytecodeVersionV6);
+  auto bytecode_tuple = c10::ivalue::Tuple::create(std::move(bytecode_values));
+  SerializationStorageContext storage_context;
+  writeArchiveV5(
+      writer_bytecode,
+      c10::ivalue::Tuple::create(constants_values),
+      /*archive_name=*/"constants",
+      /*archive_dir=*/"",
+      /*tensor_dir=*/"constants/",
+      /*use_storage_context=*/true,
+      storage_context);
+  writeArchiveV5(
+      writer_bytecode,
+      bytecode_tuple,
+      /*archive_name=*/"bytecode",
+      /*archive_dir=*/"",
+      /*tensor_dir=*/"constants/",
+      /*use_storage_context=*/true,
+      storage_context);
+
+  return ouput_model_stream;
+}
+
 } // namespace
 
 // A generic contract for backport logic to the previous bytecode version.
@@ -362,6 +454,7 @@ using BytecodeBackportFunction =
 BackportManager::BackportManager() {
   registerBytecodeBackportFunction(kBytecodeVersionV5, backport_v5_to_v4);
   registerBytecodeBackportFunction(kBytecodeVersionV6, backport_v6_to_v5);
+  registerBytecodeBackportFunction(kBytecodeVersionV7, backport_v7_to_v6);
 }
 
 std::unordered_map<
